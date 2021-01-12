c78a0fe5bac8ed349ab360b29403ef13eb550d00
mavis <nanglaokhay2015@gmail.com>
2021-01-12 18:00:06 +0900

    coroution のasync,await,waiting,cancelation

--- app/src/main/java/com/example/imageloadmoreview/MainActivity.kt
+++ app/src/main/java/com/example/imageloadmoreview/MainActivity.kt
@@ -1,37 +1,35 @@
 package com.example.imageloadmoreview
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?): String {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //coroutines
-       val job = GlobalScope.launch(Dispatchers.Default) {
-           repeat(5){
-               delay(1000L)
-               }
-            Log.d(this@MainActivity.toString(), "Coroutine says hello from thread ${Thread.currentThread().name}")
-        }
-        Log.d(this@MainActivity.toString(), "Hello from thread ${Thread.currentThread().name}")
-
-        Log.d(this@MainActivity.toString(), "Before RunBlocking")
-        runBlocking {
-            job.join()
-            job.cancel()
-            launch(Dispatchers.IO) {
-                delay(3000L)
+        //Async and Await kotlin coroutines
+        GlobalScope.launch {
+            val time = measureTimeMillis {
+                val answer1 = async { networkCall1() }
+                val answer2 = async { networkCall2() }
+                Log.d(this@MainActivity.toString(), "Answer1:${answer1.await()}")
+                Log.d(this@MainActivity.toString(), "Answer2:${answer2.await()}")
             }
-            Log.d(this@MainActivity.toString(), "start RunBlocking")
-            delay(100L)
-            Log.d(this@MainActivity.toString(), "")
+            Log.d(this@MainActivity.toString(), "Requests took $time")
         }
-        //Thread.sleep(5000L)
-        Log.d(this@MainActivity.toString(), "")
 
-    }
+        suspend fun networkCall1() : String {
+        delay(3000L)
+        return "answer1"
+        }
+
+        suspend fun networkCall2() : String {
+            delay(5000L)
+            return "answer2"
+        }
 }
